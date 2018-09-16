f160b4f358adac3c86c59b77a5a40e74b2a38b6c
Rohan Venkatapuram <sharpedo75@gmail.com>
2018-09-16 15:47:46 -0700

    encoder stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,17 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 
 public class Auto extends LinearOpMode {
 
-    GyroSensor gyro;
+    DcMotor motor;
 
+    final double NUM_OF_TICKS = 500;
+    final double CIRCUMFERENCE = 10;
     public void runOpMode()
     {
-        gyro.calibrate();
-        while(gyro.isCalibrating());
-        gyro.getHeading();
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setTargetPosition((int)(12 / CIRCUMFERENCE * NUM_OF_TICKS));
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(0.5);
+        while (motor.isBusy());
+        motor.setPower(0);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
     }
 
 }
