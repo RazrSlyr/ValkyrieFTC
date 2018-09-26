c9080b4d5be55d935a85aa28ec58b82e4b7a085a
Rohan Venkatapuram <sharpedo75@gmail.com>
2018-09-25 18:31:19 -0700

    made basic driving teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -8,16 +8,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOp extends OpMode {
 
-    ColorSensor cSensor;
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+    DcMotor rightFront;
 
 
 
     public void init()
     {
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+
     }
 
     public void loop()
     {
+        double ly = -gamepad1.left_stick_y;
+        double ry = -gamepad1.right_stick_y;
+        leftBack.setPower(ly);
+        leftFront.setPower(ly);
+
+        rightBack.setPower(ry);
+        rightFront.setPower(ry);
+
 
     }
 }
